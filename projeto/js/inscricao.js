623a57ffdeb719115079e5d31b5da6a97bd32e9a
Cristian Edson Meneghetti <cristian.meneghetti@pampa.compasso>
2019-06-06 17:37:15 -0300

    Atividade 4 - melhorias nas condições de validação do formulário

--- projeto/js/inscricao.js
+++ projeto/js/inscricao.js
@@ -1,4 +1,19 @@
+function confirmaValidade(){
+    let botao = $("#submit")[0];
 
+   if(ehNomeValido  && ehEmailValido && ehParticipacaoValido &&
+     ehDisponibilidadeValido && ehInteressesValido){
+        botao.classList.add("botao-enviar-ativado");
+     }
+     else{
+         if(botao.classList.contains("botao-enviar-ativado")){
+            botao.classList.remove("botao-enviar-ativado");
+         }
+     }
+
+     console.log(ehNomeValido  && ehEmailValido && ehParticipacaoValido &&
+        ehDisponibilidadeValido && ehInteressesValido);
+};
  
         let nome = $("input[name='nome']");
         let email = $("input[name='email']");
@@ -7,61 +22,91 @@
         let interesses = $("fieldset[name='interesses']").children().find('input');
         let participacao = $("textarea[name='participacao']");
 
-        let isNomeValido = false;
-        let isEmailValido = false;
-        let isDisponibilidadeValido = false;
-        let isInteressesValido = false;
-        let isParticipacaoValido = false;
+        let interessesNumero = 0;
+
+        let ehNomeValido = false;
+        let ehEmailValido = false;
+        let ehDisponibilidadeValido = false;
+        let ehInteressesValido = false;
+        let ehParticipacaoValido = false;
         
         let submitDisponivel = false;
+        let regexp = /^[a-zA-Z\s]*$/;
 
-        $(nome).change((event) => {
+        $(nome).keypress((event) => {            
             let campo = event.target;
-            let regex = new RegExp("\D*");
-            if(regex.test(campo.value) &&
+            console.log(campo.value);
+            console.log(regexp.test(campo.value));
+            if(regexp.test(campo.value) &&
             campo.value.split(/\S+/).length-1 >= 2){
-                isNomeValido = true;
+                ehNomeValido = true;
             }
             else{
-                isNomeValido = false;
+                ehNomeValido = false;
             }
+            confirmaValidade()
         });
 
         $(email).keypress((event) => {
             let campo = event.target;
-            isEmailValido = campo.validityState.valid;
+            ehEmailValido = campo.validity.valid;
+            confirmaValidade()
         });
 
         $(disponibilidade).change((event) => {
-            let campo = event.target;
+            let campo = event.target;            
             if(campo.value == "personalizar"){
-                let disponibilidade = $("input[name='personalizar']");
-                if(disponibilidade.val().length == 0){
-                    isDisponibilidadeValido = false;
+                let disponibilidade = $("input[name='personalizar']")[0];
+                disponibilidade.required = true;
+                if(disponibilidade.value.length == 0){
+                    ehDisponibilidadeValido = false;
+                    confirmaValidade();
                     return;
                 }                
+            }
+            else{
+                let disponibilidade = $("input[name='personalizar']")[0];
+                disponibilidade.value = "";
+                disponibilidade.required = false;
             }            
-            isDisponibilidadeValido = true;
+            ehDisponibilidadeValido = true;
+            confirmaValidade()
         });
 
         $(interesses).change((event) => {
             let campo = event.target;
-            if(campo.value == "outro"){
-                let interesse = $("input[name='outro-interesse']");
-                if(interesse.val().length == 0){
-                    isInteressesValido = false;
-                    return;
-                }                
-            }            
-            isInteressesValido = true;
-        });
-
-        $(participacao).change((event) => {
-            let target = event.target;
-            if(target.value.length <= 140){
-                isParticipacaoValido = false;                
+            if(campo.checked){
+                interessesNumero += 1;
             }
             else{
-                isParticipacaoValido = true;
+                interessesNumero -= 1;
             }
+            if(campo.value == "outro"){                
+                let interesse = $("input[name='outro-interesse']")[0];
+                interesse.required = true;
+                if(interesse.value.length == 0 && interessesNumero == 1){
+                    interesse.value = "";
+                    ehInteressesValido = false;
+                    confirmaValidade();
+                    return;
+                }
+                else{
+                    let interesse = $("input[name='outro-interesse']")[0];
+                    interesse.value = "";
+                    interesse.required = false;
+                }                
+            }            
+            ehInteressesValido = true;
+            confirmaValidade()
+        });
+
+        $(participacao).keypress((event) => {
+            let target = event.target;
+            if(target.value.length <= 140){
+                ehParticipacaoValido = false;                
+            }
+            else{
+                ehParticipacaoValido = true;
+            }
+            confirmaValidade()
         });
