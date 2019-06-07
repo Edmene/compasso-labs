8d4f0967f879468dc78dac3c0e302f81b63f25d8
Cristian Edson Meneghetti <cristian.meneghetti@pampa.compasso>
2019-06-07 17:21:23 -0300

    Atividade 4 - Ajustes na validação e remoção do formulário

--- projeto/js/inscricao.js
+++ projeto/js/inscricao.js
@@ -13,27 +13,75 @@ class Inscricao {
         this._participacao = this.query("textarea[name='participacao']")[0];
 
         this._regexp = /^[a-zA-ZâÂãÃçóòáàüẑéèêõÕ\s]*$/;
-        this._interesseNumero = {n : 0};
+        this._regexpEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+        this._interesseNumero = {n: 0};
+        this._valido = {valid: false};
 
         this._init();
         Object.freeze(this);
     }
 
+    get valid(){
+        new Object(this._valido);
+    }
+
     _init(){
 
         this._nome.valido = false;
+        this._email.valido = false;
+        this._nome.classList.add("campo-focus");
         this._disponibilidade.valido = false;
         this._interesses.valido = false;
         this._participacao.valido = false;
 
+        this.adicionaFocus(this._nome);
+        this.adicionaFocus(this._participacao);
+        this.adicionaFocus(this._email);
+
+        this._email.addEventListener(
+            "input", (event) => {
+                let campo = event.target;
+                campo.classList.remove("campo-focus");
+                if(this._regexpEmail.test(campo.value)){
+                    this._email.valido = true;
+                    if(this._email.classList.contains("campo-invalido")){
+                        this._email.classList.remove("campo-invalido");
+                        this._email.classList.add("campo-valido");
+                    }
+                }
+                else{
+                    if(!this._email.classList.contains("campo-invalido")){
+                        this._email.classList.add("campo-invalido");
+                        this._email.classList.remove("campo-valido");
+                    }
+                    this._email.valido = false;
+                }
+            }
+        )
+        
+
         this._nome.addEventListener(
             "input", (event) => {
             let campo = event.target;
+            if(campo.value.length == 0){
+                campo.classList.add("campo-focus");
+            }
+            else{
+                campo.classList.remove("campo-focus");
+            }
             if(this._regexp.test(campo.value) &&
                 campo.value.split(/\S+/).length-1 >= 2){
                 this._nome.valido = true;
+                if(this._nome.classList.contains("campo-invalido")){
+                    this._nome.classList.remove("campo-invalido");
+                    this._nome.classList.add("campo-valido");
+                }
             }
             else{
+                if(!this._nome.classList.contains("campo-invalido")){
+                    this._nome.classList.add("campo-invalido");
+                    this._nome.classList.remove("campo-valido");
+                }
                 this._nome.valido = false;
             }
         });
@@ -90,74 +138,104 @@ class Inscricao {
 
         this._participacao.addEventListener(
             "input", (event) => {
-            let target = event.target;
-            if(target.value.length <= 140){
+            let campo = event.target;
+            if(campo.value.length == 0){
+                campo.classList.add("campo-focus");
+            }
+            else{
+                campo.classList.remove("campo-focus");
+            }
+            if(campo.value.length <= 140){
                 this._participacao.valido = false;
+                if(!this._participacao.classList.contains("campo-invalido")){
+                    this._participacao.classList.add("campo-invalido");
+                    this._participacao.classList.remove("campo-valido");
+                }
             }
             else{
                 this._participacao.valido = true;
+                if(this._participacao.classList.contains("campo-invalido")){
+                    this._participacao.classList.remove("campo-invalido");
+                    this._participacao.classList.add("campo-valido");
+                }
             }
         });
 
         this._document.querySelector("form").addEventListener(
-            "input", (event) => this.validade(event)
+            "input", (event) => {this.validade(event)}
         );
 
-        this._form.addEventListener("any", event => event.preventDefault());
-        this._form.addEventListener("submit", event => this.formInvalidoEvento(event));
+        form.addEventListener("submit", event => this.formValidoEvento(event));
     }
 
-    formInvalidoEvento(event){
-        event.preventDefault();
-        let p = $("<p>").text("Favor preencher os campos");
-        let modal = $("<div>");
-        modal[0].appendChild(p[0]);
-        modal.addClass("aviso-preencher");
-        modal[0].addEventListener("click", event => {
-            let target = event.target;
-            if(target.nodeName == 'P'){
-                target.parentNode.remove();
-            }
-            else{
-                target.remove();
-            }
+    formConfirmacao(dados){
+        this._form.remove();
+        this.query(".container-form p")[0].remove()
 
-        });
-        this.query('body')[0].appendChild(modal[0]);
+        let confirmacao = $("<div>");
+        let p = $("<p>").text(`Inscrição enviada. Aguarde detalhes em seu e-mail ${dados.email}`);
+        let a = $("<a>");
+        a[0].innerText = "fazer outra inscrição";
+        a[0].href = "inscricao.html";
+        
+        confirmacao[0].appendChild(p[0]);
+        confirmacao[0].appendChild(a[0]);
+        confirmacao.addClass("confirmacao");        
+        this.query('.container-form')[0].appendChild(confirmacao[0]);
+        
     }
 
     formValidoEvento(event){
         event.preventDefault();
-        let nome = this._nome.value;
-        let email = this._email.value;
-        let disponibilidades = this._selecaoDisponivel;
-        let disponibilidade = "";
-        disponibilidades.forEach(item => {
-            if(item.checked) {
-                disponibilidade = item.value;
-            }
-        });
-        let interesses = this._interesses.querySelectorAll("input");
-        let interesse = [];
-        interesses.forEach(item => {
-            if(item.checked){
-                interesse.push(item.value);
+        if(this._valido.valid){
+
+            let nome = this._nome.value;
+            let email = this._email.value;
+            let disponibilidades = this._selecaoDisponivel;
+            let disponibilidade = "";
+            disponibilidades.forEach(item => {
+                if(item.checked) {
+                    disponibilidade = item.value;
+                }
+            });
+            let interesses = this._interesses.querySelectorAll("input");
+            let interesse = [];
+            interesses.forEach(item => {
+                if(item.checked && item.value != "outro"){
+                    interesse.push(item.value);
+                }
+                else{
+                    if(item.type == "text"){
+                        interesse[interesse.indexOf({"outro": ""})] = {"outro": item.value};
+                    }
+                }
+            });
+            let participacao = this._participacao.value;
+            let personalizar = this._disponibilidade.querySelector(".input-oculto").value;
+
+            let dadosInscricao = new DadosInscricao(nome, email, disponibilidade,
+                interesse, participacao, personalizar);
+
+            this._form.reset();
+            this._interesseNumero.n = 0;
+            this.formConfirmacao(dadosInscricao);
+
+            console.log(dadosInscricao);
+        }
+    }
+
+    adicionaFocus(elemento){
+        elemento.addEventListener('focus', event => {
+            if(!event.target.classList.contains("campo-valido") &&
+            !event.target.classList.contains("campo-invalido")){
+                event.target.classList.add("campo-focus");
             }
             else{
-                if(item.type == "text"){
-                    interesse[interesse.indexOf({"outro": ""})] = {"outro": item.value};
-                }
-            }
+                event.target.classList.remove("campo-focus");
+            }            
         });
-        let participacao = this._participacao.textContent;
-        let personalizar = this._disponibilidade.querySelector(".input-oculto").value;
 
-        let dadosInscricao = new DadosInscricao(nome, email, disponibilidade,
-            interesse, participacao, personalizar);
-
-        this._form.reset();
-
-        console.log(dadosInscricao);
+        elemento.addEventListener('focusout', () => elemento.classList.remove("campo-focus"));
     }
 
 
@@ -172,17 +250,14 @@ class Inscricao {
         if(valid){
             if(!this._botao.classList.contains("botao-enviar-ativado")) {
                 this._botao.classList.add("botao-enviar-ativado");
-                this._form.removeEventListener('submit', this.formInvalidoEvento);
-                this._form.addEventListener("submit", event => this.formValidoEvento(event));
             }
         }
         else{
             if(this._botao.classList.contains("botao-enviar-ativado")){
                 this._botao.classList.remove("botao-enviar-ativado");
-                this._form.removeEventListener('submit', this.formValidoEvento);
-                this._form.addEventListener("submit", event => this.formInvalidoEvento(event));
             }
         }
+        this._valido.valid = valid;
     }
 
 }
